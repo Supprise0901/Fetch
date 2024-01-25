4a9eb99149955da28b6174abbe057affadf4f367
supprise <zzsupprise@proton.me>
2024-01-25 16:39:00 +0800

    update

--- v2ray/main.py
+++ v2ray/main.py
@@ -4,7 +4,6 @@ import schedule as schedule
 from bs4 import BeautifulSoup
 import time
 
-
 def get_code():
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
