355ce7cb8d2fe1ff9459ed9f295d1c37d064ddd6
supprise <zzsupprise@proton.me>
2024-02-01 12:44:22 +0800

    update

--- git_cmd.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-# subprocess.run(['git', 'init'])
-# subprocess.run(['git', 'branch', '-l'])
-# subprocess.run(['git', 'branch', '-m', 'master', 'main'])
-subprocess.run(['git', 'clean', '-fd'])
